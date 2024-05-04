a53ffe568ed2657e768b5b8942de8bd51aa544eb
julian piedrahita <julianpiedrahita@MacBook-Pro-de-julian.local>
2024-05-04 10:27:57 -0500

    test terraform

--- develop/infraestructure/vpc.tf
+++ develop/infraestructure/vpc.tf
@@ -9,17 +9,17 @@ module "network" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  count = local.create_vpc ? 1 : 0
+   
+  cidr_block          = var.use_ipam_pool ? null : var.cidr
+  ipv4_ipam_pool_id   = var.ipv4_ipam_pool_id
+  ipv4_netmask_length = var.ipv4_netmask_length
+   
+  assign_generated_ipv6_cidr_block     = var.enable_ipv6 && !var.use_ipam_pool ? true : null
+  ipv6_cidr_block                      = var.ipv6_cidr
+
   enable_nat_gateway = true
   single_nat_gateway = true
-
-  enable_flow_log                      = true
-  create_flow_log_cloudwatch_log_group = true
-  create_flow_log_cloudwatch_iam_role  = true
-
-  flow_log_max_aggregation_interval         = 60
-  flow_log_cloudwatch_log_group_name_prefix = "/aws/my-amazing-vpc-flow-logz/"
-  flow_log_cloudwatch_log_group_name_suffix = "my-test"
-  flow_log_cloudwatch_log_group_class       = "INFREQUENT_ACCESS"
 
   tags = {
     Terraform   = "true"

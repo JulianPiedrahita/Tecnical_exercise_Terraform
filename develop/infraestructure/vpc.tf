efc2e713562017a73366eaa47ad892e80f20e184
julian piedrahita <julianpiedrahita@MacBook-Pro-de-julian.local>
2024-05-04 10:44:08 -0500

    test terraform

--- develop/infraestructure/vpc.tf
+++ develop/infraestructure/vpc.tf
@@ -1,7 +1,28 @@
+resource "aws_vpc" "Vpc_Dev" {
+  count = local.create_vpc ? 1 : 0
+
+  cidr_block                       = var.cidr_block
+  ipv4_ipam_pool_id                = var.vpc_ipv4_ipam_pool_id
+  ipv4_netmask_length              = var.vpc_ipv4_netmask_length
+  assign_generated_ipv6_cidr_block = var.vpc_assign_generated_ipv6_cidr_block
+  ipv6_cidr_block                  = var.vpc_ipv6_cidr_block
+  ipv6_ipam_pool_id                = var.vpc_ipv6_ipam_pool_id
+  ipv6_netmask_length              = var.vpc_ipv6_netmask_length
+
+  enable_dns_hostnames = var.vpc_enable_dns_hostnames
+  enable_dns_support   = var.vpc_enable_dns_support
+  instance_tenancy     = var.vpc_instance_tenancy
+
+  tags = merge(
+    { "Name" = var.name },
+    module.tags.tags_aws
+  )
+}
+
 module "network" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
-
+  
   name = "web"
   cidr = "10.0.0.0/16"
 
